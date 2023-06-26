87b4e99144b20ec5bb08ee41b09a92de85019de8
joeyclemens <joeyclemens87@gmail.com>
2023-06-26 10:26:22 +0100

    ;;

--- cohbra.py
+++ cohbra.py
@@ -20,7 +20,7 @@ def create_line_chart(df, title):
         y=alt.Y('Actual', sort=None, title='Targets'),
         tooltip=[alt.Tooltip('Dates', title='Date'), alt.Tooltip('Actual', title='Target')]
     ).properties(
-        width=2000  # Set the chart width to 900 pixels
+        width=1400  # Set the chart width to 900 pixels
     )
 
     # Add target line to chart
@@ -104,7 +104,7 @@ room_loading_audit = pd.read_csv('audit/room_loading_audit_progress.csv')
 # Add textbox to sidebar
 st.sidebar.markdown("<u>Links:</u>", unsafe_allow_html=True)
 # Add hyperlink to sidebar
-st.sidebar.write('<a href="https://mjmedical.sharepoint.com/:x:/s/COHBRA/Eb4o_yhqVspHjEDxC7hptYYBy4ryOXYl8nmXwHW0wT12Vw?e=tzJZid" style="color: black; text-decoration: none;"><b>KPI Tracker</b></a>', unsafe_allow_html=True)
+st.sidebar.write('<a href="https://mjmedical.sharepoint.com/:x:/s/COHBRA/EeyfkMUA6vpBv_03OhwQUI0BS7iEPk1le9DfTPkhMBLGBA?e=piBaKY&nav=MTVfezM0ODJDMUUyLUNBMUItNEM5NC04RkVCLUU4QUY5Njc4MkYyOX0" style="color: black; text-decoration: none;"><b>KPI Tracker</b></a>', unsafe_allow_html=True)
 # Add a dividing line
 st.sidebar.markdown("<hr>", unsafe_allow_html=True)
 
@@ -117,7 +117,9 @@ st.sidebar.markdown("<hr>", unsafe_allow_html=True)
 
 # Add selectbox to choose which graph and table to show
 option = st.sidebar.selectbox('Select an option',
-                             ['Room Progress','Progress','Audit'])
+                             ['Room Progress','Progress',
+                              #'Audit'
+                              ])
 
 # Room progress drop down options
 if option == 'Room Progress':
@@ -183,33 +185,33 @@ elif option == 'Progress':
 
 
 
-elif option == 'Audit':
-    charts = {
-        'Equipment planning': data_audit,
-        'Activities': activities_audit,
-        'Room Loading': room_loading_audit,
-    }
+#elif option == 'Audit':
+#    charts = {
+#        'Equipment planning': data_audit,
+#        'Activities': activities_audit,
+#        'Room Loading': room_loading_audit,
+#    }
 
-    chart_choice = st.sidebar.selectbox('Choose chart', list(charts.keys()))
+#    chart_choice = st.sidebar.selectbox('Choose chart', list(charts.keys()))
 
-    selected_chart = charts[chart_choice]
+#    selected_chart = charts[chart_choice]
     
-        # Find the latest actual value
-    latest_actual = selected_chart['Actual'].dropna().iloc[-1]
+#        # Find the latest actual value
+#    latest_actual = selected_chart['Actual'].dropna().iloc[-1]
 
     # Find the latest target value
-    latest_target = selected_chart['Target'].dropna().iloc[-1]
+#    latest_target = selected_chart['Target'].dropna().iloc[-1]
 
     # Calculate completion percentage
-    completion_percentage = (latest_actual / latest_target) * 100
+#    completion_percentage = (latest_actual / latest_target) * 100
 
     # Convert completion percentage to fraction
-    completion_fraction = completion_percentage / 100.0
+#    completion_fraction = completion_percentage / 100.0
 
     # Display progress bar
-    st.progress(completion_fraction)
+#    st.progress(completion_fraction)
     
-    create_line_chart(selected_chart, chart_choice)
+#    create_line_chart(selected_chart, chart_choice)
 
     
 #Various tables drop down options

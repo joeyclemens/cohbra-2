156c3ee750fdb129bc54438167ac8dcfb9b8535f
Joey Clemens <joeyclemens87@gmail.com>
2023-09-25 10:27:49 +0100

    f

--- cohbra.py
+++ cohbra.py
@@ -100,16 +100,8 @@ room_loading_audit = pd.read_csv('audit/room_loading_audit_progress.csv')
 
 # Add textbox to sidebar
 st.sidebar.markdown("<u>Links:</u>", unsafe_allow_html=True)
-
-# Add hyperlink to KPI Tracker
+# Add hyperlink to sidebar
 st.sidebar.write('<a href="https://mjmedical.sharepoint.com/:x:/s/COHBRA/EeyfkMUA6vpBv_03OhwQUI0BS7iEPk1le9DfTPkhMBLGBA?e=piBaKY&nav=MTVfezM0ODJDMUUyLUNBMUItNEM5NC04RkVCLUU4QUY5Njc4MkYyOX0" style="color: black; text-decoration: none;"><b>KPI Tracker</b></a>', unsafe_allow_html=True)
-
-# Add hyperlink to Database (local file)
-file_path = "file:///F:/MJM Documents/Projects/Template/Products/Database/MJM_Template_181119.accdb"
-st.sidebar.write(f'<a href="{file_path}" style="color: black; text-decoration: none;"><b>Database</b></a>', unsafe_allow_html=True)
-
-# Add a dividing line
-st.sidebar.markdown("<hr>", unsafe_allow_html=True)
 # Add a dividing line
 st.sidebar.markdown("<hr>", unsafe_allow_html=True)
 
@@ -268,7 +260,7 @@ header {visibility: hidden;}
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 style_css = """
-<link rel="stylesheet" href="static/style.css">
+<link rel="stylesheet" href="static\style.css">
 """ 
 st.markdown(style_css, unsafe_allow_html=True)
 
